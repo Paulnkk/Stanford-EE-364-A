b35170aacb0def91e8b30b2c298b2a62250cc11b
Paul <93285862+Paulnkk@users.noreply.github.com>
2022-08-27 14:59:25 -0400

    Update allocating_memory.py

--- allocating_memory.py
+++ allocating_memory.py
@@ -6,9 +6,10 @@ from allocate_memory_data import C,D,c,b
 m=256#number of banks
 n=8#number of cores
 M=cp.Variable((n,m),nonneg=True)
-constraints=[M@np.ones((m,))==b,
-M.T@np.ones((n,))<=c]
-objective=cp.Minimize(cp.vec(M).T@cp.vec(C)+cp.vec(cp.power(M, 2))@cp.vec(D))
-problem=cp.Problem(objective,constraints)
+
+constraints = [M@np.ones((m,))==b, M.T@np.ones((n,))<=c]
+objective = cp.Minimize(cp.vec(M).T@cp.vec(C)+cp.vec(cp.power(M, 2))@cp.vec(D))
+
+problem = cp.Problem(objective,constraints)
 problem.solve()
-plot_memory(M.value
+plot_memory(M.value)
